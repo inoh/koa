8f201c44071b64a5f8fd331c8d79aafaf71a348e
inoh <syslink.h.inoue@gmail.com>
2023-12-10 13:29:41 +0900

    Typescript に対応

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-const Koa = require('koa');
+import Koa from 'koa'
+
 const app = new Koa();
 
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  ctx.body = 'Hello World!';
 });
 
 app.listen(6000);
